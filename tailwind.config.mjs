c0f234c16ee6d4012e0ea3dfc5acca4817e9772e
Eduardo R <simplyy017@gmail.com>
2024-07-22 15:52:47 -0500

    Instalación de fuentes y colores personalizados

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        bgCard: {
+          addPlant: "#C6F2ED",
+          allGood: "#E2FF6E",
+          alert: "#FFED8C",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
